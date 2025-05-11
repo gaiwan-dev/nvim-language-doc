972b4ce030437437a1abc39a264ba359964a2268
pygaiwan <moise.medici@gmail.com>
2025-05-11 15:09:52 +0100

    fix: get context word instead of clipboard buffer

--- lua/nvim-language-doc/init.lua
+++ lua/nvim-language-doc/init.lua
@@ -1,15 +1,17 @@
 -- TODO: lifecycle del buffer? meaning, do i need to close/destroy the buffer after use?
+-- expose the config and actually use it
+-- add a panel like `sg` to enter a specific method/class without having to rely on cursor
 local config = require("nvim-language-doc.config")
+
 local M = {}
 
 ---Open help window on the right side
-function M.open_help_window(cmd)
-	local arg = vim.fn.getreg('"')
-	print(arg)
-	print(cmd)
+function M._open_help_window(cmd)
+	local arg = vim.fn.expand("<cword>")
 	local buffnr = vim.api.nvim_create_buf(true, true)
 
 	local cmd_content = vim.fn.system(cmd .. arg)
+
 	vim.api.nvim_buf_set_lines(buffnr, 0, -1, false, vim.split(cmd_content, "\n"))
 	local win_id = vim.api.nvim_open_win(buffnr, true, {
 		split = "right",
@@ -23,22 +25,28 @@ function M.open_help_window(cmd)
 end
 
 ---return the command to view the docstring based on the LSP attached
----@return string
-function M.get_cmd_by_lsp()
+---@return string?
+function M._get_cmd_by_lsp()
 	local lsps = vim.lsp.get_clients()
 	local lsp_configured = config.get_default_config().default
+
 	for _, lsp in ipairs(lsps) do
 		local name = lsp.name
 		local helper_cmd = lsp_configured[name]
 
 		if helper_cmd then
-			print(helper_cmd)
 			return helper_cmd
 		end
 	end
-	error("Failed to find a helper command for the language you are using. Add it to the config.")
+	print("Failed to find a helper command for the language you are using. Add it to the config.")
+	return nil
 end
 
--- local helper_cmd = get_cmd_by_lsp()
--- open_help_window(helper_cmd)
+function M.execute()
+	local cmd = M._get_cmd_by_lsp()
+	if cmd ~= nil then
+		M._open_help_window(cmd)
+	end
+end
+
 return M

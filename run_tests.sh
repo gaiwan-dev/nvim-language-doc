a304e1f323af547f1de6201e0f4d3d0bbdcf607a
gaiwan-dev <moise.medici@gmail.com>
2025-05-17 15:27:23 +0100

    test: move run_tests to use sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 nvim --headless -u scripts/minimal_init.lua -c \
   "lua require('plenary.busted').run('lua/nvim-language-doc/test/python_spec.lua', {minimal_init = 'scripts/minimal_init.lua'})"
